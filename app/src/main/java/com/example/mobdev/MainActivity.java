85a3cc4e6f97517db91e0106b8b3a84c08d58cc2
StephenSales <sdsalesstpn@gmail.com>
2024-05-25 15:08:07 +0800

    s

--- app/src/main/java/com/example/mobdev/MainActivity.java
+++ app/src/main/java/com/example/mobdev/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mobdev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSignin = findViewById(R.id.btnSignin);
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, home.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
