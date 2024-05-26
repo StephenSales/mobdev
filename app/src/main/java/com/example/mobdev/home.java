00835e92fd63a71eeb60a4e21f4d0699ee0dea11
StephenSales <sdsalesstpn@gmail.com>
2024-05-27 01:17:11 +0800

    s

--- app/src/main/java/com/example/mobdev/home.java
+++ app/src/main/java/com/example/mobdev/home.java
@@ -1,8 +1,12 @@
 package com.example.mobdev;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageButton btnExplore = findViewById(R.id.btnExplore);
+        ImageButton btnEvents = findViewById(R.id.btnEvents);
+        ImageButton btnCreate = findViewById(R.id.btnCreate);
+        ImageButton btnBookmark = findViewById(R.id.btnBookmark);
+        ImageButton btnProfile = findViewById(R.id.btnProfile);
+
+        btnExplore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment homepage = new homepage();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, homepage)
+                        .commit();
+            }
+        });
+
+        btnEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment events = new events();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, events)
+                        .commit();
+            }
+        });
     }
 }
