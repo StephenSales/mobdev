b3386dc97c46387a386b9c8fbf0bf019019dcb09
StephenSales <sdsalesstpn@gmail.com>
2024-05-27 08:53:55 +0800

    s

--- app/src/main/java/com/example/mobdev/myprofile.java
+++ app/src/main/java/com/example/mobdev/myprofile.java
@@ -3,6 +3,9 @@ package com.example.mobdev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class myprofile extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class myprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
+
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
