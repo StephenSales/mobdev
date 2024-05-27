3b7fce1eda94d1e12673b146cf2135f4d1299507
Roddneil Gemina <roddneilgemina@gmail.com>
2024-05-27 13:44:18 +0800

    ADDED OTHER PROFILE

--- app/src/main/java/com/example/mobdev/profile.java
+++ app/src/main/java/com/example/mobdev/profile.java
@@ -9,5 +9,6 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        char y;
     }
 }
