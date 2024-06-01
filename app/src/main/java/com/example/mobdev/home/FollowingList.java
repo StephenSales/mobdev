7cd835433028bc04d79136287a393175cc84336a
Roddneil Gemina <roddneilgemina@gmail.com>
2024-06-01 16:20:15 +0800

    Edited Profile and added Following/Follower

---
+++ app/src/main/java/com/example/mobdev/home/FollowingList.java
@@ -0,0 +1,16 @@
+package com.example.mobdev.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobdev.R;
+
+public class FollowingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_following_list);
+    }
+}
